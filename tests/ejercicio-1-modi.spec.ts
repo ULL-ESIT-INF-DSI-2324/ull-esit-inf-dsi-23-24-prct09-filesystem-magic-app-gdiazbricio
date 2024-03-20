034c2ff93d5f060386d67b25a83e55a1d236cd49
Guille Díaz <alu0101505688@ull.edu.es>
2024-03-20 11:26:53 +0000

    added instances folder

--- tests/ejercicio-1-modi.spec.ts
+++ tests/ejercicio-1-modi.spec.ts
@@ -8,28 +8,28 @@ import { ExtractInfoInJSON } from "../src/ExtractInfoInJSON.js";
 
 describe("exercise 1 tests", () => {
   it("must return a tuple containing weights and profits reading from CSV", () => {
-    const myExtraction = new ExtractInfoInCSV("bag.csv");
+    const myExtraction = new ExtractInfoInCSV("instances/bag.csv");
   myExtraction.run();
   const myInfo: Solution = myExtraction.solution_;
     expect(myInfo).to.be.deep.equal([ [ '3', '1', '2', '2' ], [ '4', '2', '2', '5' ] ]);
   });
 
   it("must return a tuple containing weights and profits reading from CSV", () => {
-    const myExtraction = new ExtractInfoInCSV("bag2.csv");
+    const myExtraction = new ExtractInfoInCSV("instances/bag2.csv");
   myExtraction.run();
   const myInfo: Solution = myExtraction.solution_;
     expect(myInfo).to.be.deep.equal([ [ '3', '1' ], [ '4', '2' ] ]);
   });
 
   it("must return a tuple containing weights and profits reading from JSON", () => {
-    const myExtraction = new ExtractInfoInJSON("bag.json");
+    const myExtraction = new ExtractInfoInJSON("instances/bag.json");
   myExtraction.run();
   const myInfo: Solution = myExtraction.solution_;
     expect(myInfo).to.be.deep.equal([ [ '3', '1', '2', '2' ], [ '4', '2', '2', '5' ] ]);
   });
 
   it("must return a tuple containing weights and profits reading from JSON", () => {
-    const myExtraction = new ExtractInfoInJSON("bag2.json");
+    const myExtraction = new ExtractInfoInJSON("instances/bag2.json");
   myExtraction.run();
   const myInfo: Solution = myExtraction.solution_;
     expect(myInfo).to.be.deep.equal([ [ '3', '1' ], [ '4', '2' ] ]);
